a1ef3fa55b6b42fb6553653b00268dc93d210731
abhispaceX <abhiramgaddam13@gmail.com>
2024-10-21 14:08:34 +0530

    changed search box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ const WeatherDashboard: React.FC = () => {
               </label>
               <div className="relative">
                 <Select onValueChange={handleCityChange} defaultValue={city}>
-                  <SelectTrigger id="city-select" className="w-full bg-white border-2 border-blue-300 rounded-lg py-2 pl-3 pr-10 text-left cursor-default focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                  <SelectTrigger id="city-select" className="w-full bg-white border-2 border-blue-300 rounded-lg py-2 pl-3  text-left cursor-default focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                     <div className="flex items-center">
                       <MapPin className="flex-shrink-0 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
                       <SelectValue placeholder="Choose a city" />
@@ -159,11 +159,6 @@ const WeatherDashboard: React.FC = () => {
                     ))}
                   </SelectContent>
                 </Select>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                  <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </div>
               </div>
             </div>
 
